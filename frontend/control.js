311af4649707d7a5d640bd02d355b414a21c806b
balajiravichandar19-source <balajiravichandar19@gmail.com>
2026-02-07 23:10:29 +0530

    new changes in frontend

--- frontend/control.js
+++ frontend/control.js
@@ -1,25 +1,26 @@
-function checkLoan() {
-  fetch("http://127.0.0.1:5000/predict", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      Gender: 1,              // Male=1, Female=0
-      Married: 1,             // Yes=1, No=0
-      Dependents: 1,
-      Education: 0,           // Graduate=0, Not Graduate=1
-      Self_Employed: 0,       // Yes=1, No=0
-      ApplicantIncome: 5000,
-      CoapplicantIncome: 1500,
-      LoanAmount: 150,
-      Loan_Amount_Term: 360,
-      Credit_History: 1,
-      Property_Area: 2        // Urban=2, Semiurban=1, Rural=0
-    })
-  })
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("result").innerHTML =
-      `Loan Status: ${data.Loan_Status}<br>
-       EMI: ₹${data.Estimated_EMI}`;
-  });
+async function calculate() {
+
+    let loan = document.getElementById("loan").value;
+    let rate = document.getElementById("rate").value;
+    let time = document.getElementById("time").value;
+
+    let response = await fetch("http://127.0.0.1:5000/predict", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            loan: loan,
+            rate: rate,
+            time: time
+        })
+    });
+
+    let data = await response.json();
+
+    document.getElementById("current").innerText =
+        "Current Interest: " + data.current_interest;
+
+    document.getElementById("future").innerText =
+        "Predicted Future Interest: " + data.future_interest;
 }
